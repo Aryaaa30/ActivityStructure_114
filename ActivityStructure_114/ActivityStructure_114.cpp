bf95d263e09fcf19de0dcf922292a771a70a129e
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-04-03 10:10:41 +0700

    Add project files.

---
+++ ActivityStructure_114/ActivityStructure_114.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    mahasiswa mhs1, mhs2;
+
+    mhs1.NIM = "20220140114"
+
+    
+}
+
+
