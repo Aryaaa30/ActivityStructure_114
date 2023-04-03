7e3e108c74cb5915f704836b0b116cdbeedddc15
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-04-03 11:23:02 +0700

    Memasukkan Data

--- ActivityStructure3_114/ActivityStructure3_114.cpp
+++ ActivityStructure3_114/ActivityStructure3_114.cpp
@@ -18,6 +18,21 @@ struct Mahasiswa
 int main() {
     Mahasiswa mhs[3];
 
+    for (int i = 0; i < 3; i++) {
+        cout << "Masukkan NIM :";
+        cin.getline(mhs[i].NIM,12);
+        cout << "Masukkan Nama :";
+        cin.getline(mhs[i].nama, 25);
+        cout << "Alamat :";
+        cout << "\n\tMasukkan Desa :";
+        cin.getline(mhs[i].alamat.desa, 20);
+        cout << "\tMasukkan Kota :";
+        cin.getline(mhs[i].alamat.kota, 20);
+        cout << "Masukkan Umur :";
+        cin >> mhs[i].Umur;
+        cin.ignore(1, '\n');
+    }
+
 
 
     
