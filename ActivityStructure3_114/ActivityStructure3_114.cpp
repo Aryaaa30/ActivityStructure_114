c6a4155389e932043ba3ddd5706ab3eea2115118
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-04-03 11:13:01 +0700

    Membuat Structure Data dengan tipe Data yang sama (Karakter)

--- ActivityStructure3_114/ActivityStructure3_114.cpp
+++ ActivityStructure3_114/ActivityStructure3_114.cpp
@@ -7,6 +7,14 @@ struct AlamatDetail
     char kota[20];
 };
 
+struct Mahasiswa
+{
+    char NIM[12];
+    char nama[25];
+    AlamatDetail alamat;
+    int Umur;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
